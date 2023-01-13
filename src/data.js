f7ddd40d3cd799fc659c00a68f1b963b8067ad9e
devesh1808 <devesh1808@gmail.com>
2023-01-13 14:57:02 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info:
+      "The Google Pixel is more than just another smartphone for your tasks! Its HDR+ will help you take stunning photos. The Night Sight mode with astrophotography is just what you need to click those stunning images of the star-lit skies you have been meaning to capture. This phone also features apps such as the Pixel Recorder app that transcribes speech automatically.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 16,
+    company: "SAMSUNG",
+    info:
+      "We've redesigned the Galaxy S7 and S7 edge to have memory that expands at your will. The incredibly fast processors and huge RAM keeps everything running smooth even when running at full capacity. While the bigger battery keeps you going for longer.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC 10 - Black",
+    img: "img/product-3.png",
+    price: 8,
+    company: "htc",
+    info:
+      "The HTC 10 is a beautifully crafted smartphone with a gorgeous display and an ergonomic design. This phone is the first to have optical image stabilisation for the front facing camera, which does help when shooting in low light. The phone has very snappy performance thanks to the Snapdragon 820 SoC, 4GB of RAM and 32GB of onboard storage. The USB Type-C port gets USB 3.1 Gen 1 speeds for faster data transfers. Sense UI is now leaner than ever, with many of the custom apps traded in favour of Google's native apps.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HTC 10 - White",
+    img: "img/product-4.png",
+    price: 18,
+    company: "htc",
+    info:
+      "The HTC 10 is a beautifully crafted smartphone with a gorgeous display and an ergonomic design. This phone is the first to have optical image stabilisation for the front facing camera, which does help when shooting in low light. The phone has very snappy performance thanks to the Snapdragon 820 SoC, 4GB of RAM and 32GB of onboard storage. The USB Type-C port gets USB 3.1 Gen 1 speeds for faster data transfers. Sense UI is now leaner than ever, with many of the custom apps traded in favour of Google's native apps.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "HTC Desire 626s",
+    img: "img/product-5.png",
+    price: 24,
+    company: "htc",
+    info:
+      "The HTC 10 is a beautifully crafted smartphone with a gorgeous display and an ergonomic design. This phone is the first to have optical image stabilisation for the front facing camera, which does help when shooting in low light. The phone has very snappy performance thanks to the Snapdragon 820 SoC, 4GB of RAM and 32GB of onboard storage. The USB Type-C port gets USB 3.1 Gen 1 speeds for faster data transfers. Sense UI is now leaner than ever, with many of the custom apps traded in favour of Google's native apps.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Iphone",
+    img: "img/product-6.png",
+    price: 17,
+    company: "apple",
+    info:
+      "Apple has designated the iPhone 6, released in back 2014, as a vintage product. Apple considers a product vintage when it has stopped selling them more than 5 and less than 7 years ago. Apple considers a product obsolete when it has stopped distributing them for sale more than seven years ago, and has just added the iPhone 6 to its list of vintage products",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 7",
+    img: "img/product-7.png",
+    price: 30,
+    company: "apple",
+    info:
+      "Apple has designated the iPhone 6, released in back 2014, as a vintage product. Apple considers a product vintage when it has stopped selling them more than 5 and less than 7 years ago. Apple considers a product obsolete when it has stopped distributing them for sale more than seven years ago, and has just added the iPhone 6 to its list of vintage products",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Smashed Iphone",
+    img: "img/product-8.png",
+    price: 2,
+    company: "apple",
+    info:
+      "Apple has designated the iPhone 6, released in back 2014, as a vintage product. Apple considers a product vintage when it has stopped selling them more than 5 and less than 7 years ago. Apple considers a product obsolete when it has stopped distributing them for sale more than seven years ago, and has just added the iPhone 6 to its list of vintage products",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
